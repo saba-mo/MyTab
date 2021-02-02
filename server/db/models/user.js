f9f166e2a62c602987d6c974d199a406859b7cb2
Saba Mohtasham Schuttloffel <sabamohtasham@gmail.com>
2021-02-01 16:45:36 -0800

    fix: removed duplicate fields in user model afer merging with main

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -3,12 +3,6 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const User = db.define('user', {
-  firstName: {
-    type: Sequelize.STRING,
-  },
-  lastName: {
-    type: Sequelize.STRING,
-  },
   email: {
     type: Sequelize.STRING,
     unique: true,
