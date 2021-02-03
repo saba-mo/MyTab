2887c73fb439bb91e667b3a74c6b96e50a242778
Saba Mohtasham Schuttloffel <sabamohtasham@gmail.com>
2021-02-03 09:58:21 -0800

    fix: route gets Group expenses instaed of user expenses

--- server/api/expenses.js
+++ server/api/expenses.js
@@ -1,44 +1,61 @@
 const router = require('express').Router()
 const {User, Expense, Group} = require('../db/models')
 
-// GET all of user's expenses
-router.get('/:userId', async (req, res, next) => {
+// // GET all of user's expenses
+// router.get('/:userId', async (req, res, next) => {
+//   try {
+//     const id = parseInt(req.params.userId)
+//     if (isNaN(id)) return res.sendStatus(404)
+
+//     const thisUser = await User.findByPk(id)
+//     if (!thisUser) return res.sendStatus(404)
+
+//     let expenses = await thisUser.getExpenses({
+//       attributes: ['name', 'totalCost', 'groupId'],
+//     })
+
+//     res.json(expenses)
+//   } catch (err) {
+//     next(err)
+//   }
+// })
+
+// GET all of group's expenses
+router.get('route to indivdiual group', async (req, res, next) => {
   try {
-    const id = parseInt(req.params.userId)
-    if (isNaN(id)) return res.sendStatus(404)
+    // make sure params matches what's in the individual group route
+    const groupId = parseInt(req.params.groupId)
+    if (isNaN(groupId)) return res.sendStatus(404)
 
-    const thisUser = await User.findByPk(id)
-    if (!thisUser) return res.sendStatus(404)
+    const thisGroup = await Group.findByPk(groupId)
 
-    let expenses = await thisUser.getExpenses({
-      attributes: ['name', 'totalCost', 'groupId'],
-    })
+    const groupExpenses = await thisGroup.getExpenses()
 
-    res.json(expenses)
+    res.json(groupExpenses)
   } catch (err) {
     next(err)
   }
 })
 
-// GET individual expense and show its group name
-router.get('/:userId/:expenseId', async (req, res, next) => {
-  try {
-    const expenseId = parseInt(req.params.expenseId)
-    if (isNaN(expenseId)) return res.sendStatus(404)
+// GET a user's individual expense and show its group name
+// router.get('/:userId/:expenseId', async (req, res, next) => {
+//   try {
+//     const expenseId = parseInt(req.params.expenseId)
+//     if (isNaN(expenseId)) return res.sendStatus(404)
 
-    const thisExpense = await Expense.findByPk(expenseId, {
-      attributes: ['id', 'name', 'totalCost', 'groupId'],
-      include: {
-        model: Group,
-        attributes: ['title'],
-      },
-    })
-    if (!thisExpense) return res.sendStatus(404)
+//     const thisExpense = await Expense.findByPk(expenseId, {
+//       attributes: ['id', 'name', 'totalCost', 'groupId'],
+//       include: {
+//         model: Group,
+//         attributes: ['title'],
+//       },
+//     })
+//     if (!thisExpense) return res.sendStatus(404)
 
-    res.json(thisExpense)
-  } catch (err) {
-    next(err)
-  }
-})
+//     res.json(thisExpense)
+//   } catch (err) {
+//     next(err)
+//   }
+// })
 
 module.exports = router
