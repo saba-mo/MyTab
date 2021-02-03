4d21644e794f09d833cf6506f0d712d61347cf9f
Saba Mohtasham Schuttloffel <sabamohtasham@gmail.com>
2021-02-02 16:55:16 -0800

    refactor: moved Friends from navbar to user home page

--- client/components/navigation/navbar.js
+++ client/components/navigation/navbar.js
@@ -17,7 +17,6 @@ const Navbar = ({handleClick, isLoggedIn}) => (
           <a href="#" onClick={handleClick}>
             Logout
           </a>
-          <Link to="/friends">Friends</Link>
         </div>
       ) : (
         <div>
