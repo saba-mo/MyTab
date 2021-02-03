da878d3a02da4bd132923645ee92dba82929e55f
Jess <jess.jonesr@gmail.com>
2021-02-03 12:10:51 -0500

    changes to singlegroup and clickImg

--- client/components/user/user-home.js
+++ client/components/user/user-home.js
@@ -13,27 +13,34 @@ const UserHome = (props) => {
     <div>
       {firstName ? <h3>Welcome, {firstName}!</h3> : <h3>Welcome, {email}!</h3>}
       <div className="wrapper">
-        <img
+        {/* <img
           className="groupImg"
           src="images/groupImage.png"
           alt="Second slide"
           height="400px"
-        />
-        <Link src="images/groupImage.png" to={`/groups/${userId}`}>
-          View Groups
+        /> */}
+        <Link to={`/groups/${userId}`}>
+          <img
+            className="groupImg"
+            src="images/groupImage.png"
+            alt="Second slide"
+            height="400px"
+          />
         </Link>
+        {/* <a to={`/groups/${userId}`}>
+          <img src="images/groupImage.png" />
+            </a> */}
         {/* <Groups /> */}
       </div>
       <div className="wrapper">
-        <img
-          className="groupImg"
-          src="images/friendsImage.png"
-          alt="Second slide"
-          height="400px"
-          width="407.406px"
-        />
         <Link src="images/friendsImage.png" to="/friends">
-          View Friends
+          <img
+            className="groupImg"
+            src="images/friendsImage.png"
+            alt="Second slide"
+            height="400px"
+            width="407.406px"
+          />
         </Link>
       </div>
     </div>
