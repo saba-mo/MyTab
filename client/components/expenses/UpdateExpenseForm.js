615fc9943c7f093370e4a37f2b4dc6db851dc61b
Hillary Jenkins <hello.hillary7@gmail.com>
2021-02-06 16:25:42 -0800

    refactor: dummy data decimals

--- client/components/expenses/UpdateExpenseForm.js
+++ client/components/expenses/UpdateExpenseForm.js
@@ -1,16 +1,27 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {_loadGroupMembers, _updateExpense} from '../../store'
+import currency from 'currency.js'
+
+// * [ ] added dependency currency.js so deal with currency in our database
+// * [ ] models user_group and expense deal with FLOAT, not INTEGER
+// * [ ] database now able to store dollars and cents instead of all pennies
+// * [ ] refactored groups api to reflect use of currency
+// * [ ] added note in groups api for a reminder to later add a validation
+// * [ ] moved dummydata to script file
+// * [ ] update dummydata to have decimals again
+// * [ ] update dummydata for user passwords to be password to more easily test as engineers (left two of them with secure passwords)
+// * [ ]
+const defaultState = {
+  name: '',
+  totalCost: '',
+  paidBy: '',
+}
 
 export class UpdateExpenseForm extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
-      name: props.expense.name,
-      totalCost: props.expense.totalCost,
-      paidBy: '',
-    }
-
+    this.state = defaultState
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
   }
@@ -26,30 +37,40 @@ export class UpdateExpenseForm extends React.Component {
   }
 
   handleSubmit(event) {
-    if (
-      !this.state.name ||
-      !this.state.totalCost ||
-      !this.state.paidBy ||
-      this.state.paidBy === 'select'
-    ) {
-      event.preventDefault()
-      alert('A required field is missing.')
-      return
-    }
-
-    if (!Number(this.state.totalCost)) {
-      event.preventDefault()
-      alert('Cost must be a number.')
-      return
-    }
-
     event.preventDefault()
-    this.props.toggleForm()
-    this.props.updateExpense(
-      this.props.groupId,
-      this.props.expense.id,
-      this.state
-    )
+    try {
+      if (
+        !this.state.name ||
+        !this.state.totalCost ||
+        !this.state.paidBy ||
+        this.state.paidBy === 'select'
+      ) {
+        event.preventDefault()
+        alert('A required field is missing.')
+        return
+      }
+
+      if (!Number(this.state.totalCost)) {
+        event.preventDefault()
+        alert('Cost must be a number.')
+        return
+      }
+
+      this.props.addGroupExpense(this.props.groupId, {
+        name: this.state.name,
+        totalCost: currency(this.state.totalCost).value,
+        paidBy: this.state.paidBy,
+      })
+      this.setState(defaultState)
+    } catch (error) {
+      console.log('Failed to handle expense submission due to: ', error)
+    }
+    // this.props.toggleForm()
+    // this.props.updateExpense(
+    //   this.props.groupId,
+    //   this.props.expense.id,
+    //   this.state
+    // )
   }
 
   render() {
--- script/dummyDataExpenses.js
+++ script/dummyDataExpenses.js
@@ -2,57 +2,57 @@ const expenseData = [
   [
     {
       name: 'Phascogale',
-      totalCost: 8973,
+      totalCost: 89.73,
       groupId: 8,
     },
     {
       name: 'Flycatcher',
-      totalCost: 1842,
+      totalCost: 18.42,
       groupId: 5,
     },
     {
       name: 'Rhea',
-      totalCost: 1868,
+      totalCost: 18.68,
       groupId: 7,
     },
     {
       name: 'Bush dog',
-      totalCost: 5214,
+      totalCost: 52.14,
       groupId: 1,
     },
     {
       name: 'Fisher',
-      totalCost: 898,
+      totalCost: 8.98,
       groupId: 1,
     },
     {
       name: 'Brazilian otter',
-      totalCost: 3117,
+      totalCost: 31.17,
       groupId: 2,
     },
     {
       name: 'Bateleur eagle',
-      totalCost: 9062,
+      totalCost: 90.62,
       groupId: 7,
     },
     {
       name: 'Great Egret',
-      totalCost: 8775,
+      totalCost: 87.75,
       groupId: 1,
     },
     {
       name: 'Bleeding heart monkey',
-      totalCost: 7516,
+      totalCost: 75.16,
       groupId: 8,
     },
     {
       name: `Gambel's quail`,
-      totalCost: 8884,
+      totalCost: 88.84,
       groupId: 1,
     },
     {
       name: `House Cat`,
-      totalCost: 521,
+      totalCost: 5.21,
       groupId: 1,
     },
   ],
