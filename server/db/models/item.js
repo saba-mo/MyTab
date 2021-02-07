e6402d5d563040b2a89107400d839bde33994ebc
Hillary Jenkins <hello.hillary7@gmail.com>
2021-02-06 16:30:29 -0800

    refactor: item model

--- server/db/models/item.js
+++ server/db/models/item.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
+const currency = require('currency.js')
 
 const Item = db.define('item', {
   name: {
@@ -7,9 +8,22 @@ const Item = db.define('item', {
   },
 
   cost: {
-    type: Sequelize.INTEGER,
+    type: Sequelize.FLOAT,
     validate: {
       min: 0,
+    },
+    get() {
+      // broken down of return state to more easily read as an engineer:
+      // let storedValue = this.getDataValue('totalCost')
+      // let currency = currency(storedValue)
+      // let floatValue = currency.value
+      // return floatValue
+
+      // Use Currency.js to force an appropriate currency friendly float representation of the value
+      return currency(this.getDataValue('cost')).value
+    },
+    set(value) {
+      this.setDataValue('cost', currency(value).value)
     },
   },
 
