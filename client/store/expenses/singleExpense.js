07f519ea109893c5e2b69b8aa01e2a8b05754b58
Hillary Jenkins <hello.hillary7@gmail.com>
2021-02-09 14:03:56 -0800

    chore: console.log

--- client/store/expenses/singleExpense.js
+++ client/store/expenses/singleExpense.js
@@ -37,14 +37,14 @@ export const _settleOnePortion = (paidBy, itemToSettle, groupId) => async (
   dispatch
 ) => {
   try {
-    // console.log('THUNK: whole item object: ', itemToSettle)
-    // console.log('THUNK: expense id of item: ', itemToSettle.expenseId)
-    // console.log('THUNK: amount to settle: ', itemToSettle.amount)
-    // console.log('THUNK: user to settle: ', itemToSettle.user)
-    // console.log('THUNK: boolean status of settle: ', itemToSettle.settled)
+    console.log('THUNK: whole item object: ', itemToSettle)
+    console.log('THUNK: expense id of item: ', itemToSettle.expenseId)
+    console.log('THUNK: amount to settle: ', itemToSettle.amount)
+    console.log('THUNK: user to settle: ', itemToSettle.user)
+    console.log('THUNK: boolean status of settle: ', itemToSettle.settled)
 
-    // console.log('paid by coming in: ', paidBy)
-    //settled is true, as changed in component/singleexpense.js before sending here
+    console.log('paid by coming in: ', paidBy)
+    // settled is true, as changed in component/singleexpense.js before sending here
 
     const {data} = await axios.put(
       `/api/groups/singleGroup/${groupId}/expenses/${itemToSettle.expenseId}/${itemToSettle.id}`,
