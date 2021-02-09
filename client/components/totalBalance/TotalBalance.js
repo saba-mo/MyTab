f8838e4ccb5b532cdf1cac87f5a7b6d630911b8d
Saba Mohtasham Schuttloffel <sabamohtasham@gmail.com>
2021-02-08 21:15:03 -0800

    Fix: added $ sign

--- client/components/totalBalance/TotalBalance.js
+++ client/components/totalBalance/TotalBalance.js
@@ -5,11 +5,12 @@ import {logout} from '../../store'
 const TotalBalance = () => (
   <div className="totalBalance">
     <div>Total Balance</div>
+    {/* might need to use currency.js here */}
     {/* <div>
         {positiveBalance ? (
-          <div>You are owed {balance}</div>
+          <div>You are owed ${balance}</div>
         ) : (
-          <div>You owe {balance}</div>
+          <div>You owe ${balance}</div>
         )}
       </div> */}
   </div>
