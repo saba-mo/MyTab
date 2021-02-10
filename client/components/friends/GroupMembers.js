6a4a0efc00cb0c6075372c7bbfb88278a3c04401
Saba Mohtasham Schuttloffel <sabamohtasham@gmail.com>
2021-02-10 15:15:26 -0800

    Fix: alert that you can't remove a group member no longer appears when you can

--- client/components/friends/GroupMembers.js
+++ client/components/friends/GroupMembers.js
@@ -21,9 +21,9 @@ export class GroupMembers extends React.Component {
   }
 
   // if group member has outstanding balance in the group, alert they cannot be removed, else remove them
-  attemptToRemoveMember(groupId, memberId, lengthOfMembersArray) {
+  async attemptToRemoveMember(groupId, memberId, lengthOfMembersArray) {
     this.setState({numberOfMembers: lengthOfMembersArray})
-    this.props.deleteGroupMember(groupId, memberId)
+    await this.props.deleteGroupMember(groupId, memberId)
     if (this.props.groupMembers.length === this.state.numberOfMembers) {
       alert('You cannot remove a member with a balance in the group.')
     }
