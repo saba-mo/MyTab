754d123574487748178597c10ad65b69fcd76d7e
Hillary Jenkins <hello.hillary7@gmail.com>
2021-02-10 12:21:25 -0800

    feat: portion in item table toggles settled to true, complete

--- client/store/expenses/singleExpense.js
+++ client/store/expenses/singleExpense.js
@@ -3,16 +3,10 @@ import axios from 'axios'
 /* ACTION TYPES */
 const GET_AN_EXPENSE = 'GET_AN_EXPENSE'
 const UPDATE_EXPENSE = 'UPDATE_EXPENSE'
-const SETTLE_ONE_PORTION = 'SETTLE_ONE_PORTION'
 
 /* ACTION CREATORS */
 const getAnExpense = (expense) => ({
   type: GET_AN_EXPENSE,
-  expense,
-})
-
-const settleOnePortion = (expense) => ({
-  type: SETTLE_ONE_PORTION,
   expense,
 })
 
@@ -30,33 +24,6 @@ export const _loadAnExpense = (groupId, expenseId) => async (dispatch) => {
     dispatch(getAnExpense(data))
   } catch (error) {
     console.log('Cannot find your expense because: ', error)
-  }
-}
-
-export const _settleOnePortion = (paidBy, itemToSettle, groupId) => async (
-  dispatch
-) => {
-  try {
-    // console.log('THUNK: whole item object: ', itemToSettle)
-    // console.log('THUNK: expense id of item: ', itemToSettle.expenseId)
-    // console.log('THUNK: amount to settle: ', itemToSettle.amount)
-    // console.log('THUNK: user to settle: ', itemToSettle.user)
-    // console.log('THUNK: boolean status of settle: ', itemToSettle.settled)
-
-    // console.log('paid by coming in: ', paidBy)
-    // settled is true, as changed in component/singleexpense.js before sending here
-
-    const {data} = await axios.put(
-      `/api/groups/singleGroup/${groupId}/expenses/${itemToSettle.expenseId}/${itemToSettle.id}`,
-      {
-        itemToSettle,
-      }
-    )
-    console.log('data after axios: ', data)
-
-    dispatch(settleOnePortion(data))
-  } catch (error) {
-    console.log(`The expense was not settled due to an error: `, error)
   }
 }
 
@@ -82,8 +49,6 @@ const singleExpenseReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_AN_EXPENSE:
       return action.expense
-    case SETTLE_ONE_PORTION:
-      return {...state, ...action.expense}
     case UPDATE_EXPENSE:
       return {...state, ...action.expense}
     default:
