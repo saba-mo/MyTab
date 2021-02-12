0b20fe9add12f9cab4666afc8ad6f54cb3fa2b79
Katarina Yang <yangkb09@gmail.com>
2021-02-12 16:01:49 -0500

    changed icons on nav bar and chrome tab

--- client/components/group/Groups.js
+++ client/components/group/Groups.js
@@ -27,21 +27,21 @@ import {List, Card} from 'antd'
 // ];
 
 export class Groups extends React.Component {
-  // constructor() {
-  //   super()
-  //   this.state = {showForm: false}
+  constructor() {
+    super()
+    this.state = {showForm: false}
 
-  //   this.toggleShowForm = this.toggleShowForm.bind(this)
-  //   this.handleDeleteGroup = this.handleDeleteGroup.bind(this)
-  // }
+    this.toggleShowForm = this.toggleShowForm.bind(this)
+    // this.handleDeleteGroup = this.handleDeleteGroup.bind(this)
+  }
 
   componentDidMount() {
     this.props.getGroups(this.props.user.id)
   }
 
-  // toggleShowForm() {
-  //   this.setState({showForm: !this.state.showForm})
-  // }
+  toggleShowForm() {
+    this.setState({showForm: !this.state.showForm})
+  }
 
   // handleDeleteGroup(groupId) {
   //   if (
@@ -90,33 +90,44 @@ export class Groups extends React.Component {
 
   render() {
     console.log('this.props.groups', this.props.groups)
-    return (
-      <List
-        grid={{
-          gutter: 16,
-          xs: 1,
-          sm: 2,
-          md: 4,
-          lg: 4,
-          xl: 6,
-          xxl: 3,
-        }}
-        dataSource={this.props.groups}
-        renderItem={(group) => (
-          <List.Item>
-            <Card
-              title={group.title}
-              extra={<Link to={`/groups/singleGroup/${group.id}`}>Open</Link>}
-            >
-              {[
-                <a key="list-loadmore-edit">edit</a>,
-                <a key="list-loadmore-more">remove</a>,
-              ]}
-            </Card>
-          </List.Item>
-        )}
-      />
-    )
+    if (this.props.groups.length > 0) {
+      return (
+        <List
+          grid={{
+            gutter: 16,
+            xs: 1,
+            sm: 2,
+            md: 4,
+            lg: 4,
+            xl: 6,
+            xxl: 3,
+          }}
+          dataSource={this.props.groups}
+          renderItem={(group) => (
+            <List.Item>
+              <Card
+                title={group.title}
+                extra={<Link to={`/groups/singleGroup/${group.id}`}>Open</Link>}
+              >
+                {[
+                  <a key="list-loadmore-edit">edit</a>,
+                  <a key="list-loadmore-more">remove</a>,
+                ]}
+              </Card>
+            </List.Item>
+          )}
+        />
+      )
+    } else {
+      return (
+        <div>
+          <TotalBalance />
+          <h4>See the groups you belong to, and create a new one.</h4>
+          <p>No groups to show, want to add one?</p>
+          <CreateGroupForm toggleForm={this.toggleShowForm} />
+        </div>
+      )
+    }
   }
 }
 // if (this.props.groups.length > 0) {
