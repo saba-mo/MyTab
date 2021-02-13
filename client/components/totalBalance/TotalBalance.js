06acb2bff28c535ae6350c788d3e221536fe0bc3
Katarina Yang <yangkb09@gmail.com>
2021-02-13 15:28:40 -0500

    feat(landing pg design) added cards, extended background color to cover 13inch screens

--- client/components/totalBalance/TotalBalance.js
+++ client/components/totalBalance/TotalBalance.js
@@ -2,6 +2,8 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {_loadBalance} from '../../store'
 import currency from 'currency.js'
+import {Statistic, Card, Row, Col} from 'antd'
+import {ArrowUpOutlined, ArrowDownOutlined} from '@ant-design/icons'
 
 export class TotalBalance extends React.Component {
   componentDidMount() {
@@ -10,40 +12,99 @@ export class TotalBalance extends React.Component {
 
   render() {
     const {balanceBreakdown} = this.props
-
     if (balanceBreakdown[0] === 0) {
       return (
         <div>
-          <div>Total Balance on MyTab</div>
-          <div>You are all settled up</div>
+          <Card>
+            <div>Total Balance on MyTab</div>
+            <div>You are all settled up</div>
+          </Card>
         </div>
       )
     } else if (balanceBreakdown[0] > 0) {
       return (
-        <div>
-          <div>
-            Total Balance on MyTab: {currency(balanceBreakdown[0]).format()}
-          </div>
-          <div className="positiveBalance">
-            You are owed {currency(balanceBreakdown[1]).format()}
-          </div>
-          <div className="negativeBalance">
-            You owe {currency(balanceBreakdown[2]).format()}
-          </div>
+        <div className="site-statistic-demo-card">
+          <Row gutter={16}>
+            <Col span={12}>
+              <Card>
+                <Statistic
+                  title="Total Balance on MyTab"
+                  value={currency(balanceBreakdown[0]).format()}
+                  precision={2}
+                  valueStyle={{color: '#000000'}}
+                />
+              </Card>
+            </Col>
+          </Row>
+          <Row gutter={16}>
+            <Col span={12}>
+              <Card>
+                <Statistic
+                  title="You are owed"
+                  value={currency(balanceBreakdown[1]).format()}
+                  precision={2}
+                  valueStyle={{color: 'blue'}}
+                  prefix={<ArrowUpOutlined />}
+                />
+              </Card>
+            </Col>
+          </Row>
+          <Row gutter={16}>
+            <Col span={12}>
+              <Card>
+                <Statistic
+                  title="You owe"
+                  value={currency(balanceBreakdown[2]).format()}
+                  precision={2}
+                  valueStyle={{color: 'orange'}}
+                  prefix={<ArrowDownOutlined />}
+                />
+              </Card>
+            </Col>
+          </Row>
         </div>
       )
     } else {
       return (
-        <div>
-          <div>
-            Total Balance on MyTab: {currency(balanceBreakdown[0]).format()}
-          </div>
-          <div className="positiveBalance">
-            You are owed {currency(balanceBreakdown[1]).format()}
-          </div>
-          <div className="negativeBalance">
-            You owe {currency(balanceBreakdown[2]).format()}
-          </div>
+        <div className="site-statistic-demo-card">
+          <Row gutter={16}>
+            <Col span={12}>
+              <Card>
+                <Statistic
+                  title="Total Balance on MyTab"
+                  value={currency(balanceBreakdown[0]).format()}
+                  precision={2}
+                  valueStyle={{color: '#000000'}}
+                />
+              </Card>
+            </Col>
+          </Row>
+          <Row gutter={16}>
+            <Col span={12}>
+              <Card>
+                <Statistic
+                  title="You are owed"
+                  value={currency(balanceBreakdown[1]).format()}
+                  precision={2}
+                  valueStyle={{color: 'blue'}}
+                  prefix={<ArrowUpOutlined />}
+                />
+              </Card>
+            </Col>
+          </Row>
+          <Row gutter={16}>
+            <Col span={12}>
+              <Card>
+                <Statistic
+                  title="You owe"
+                  value={currency(balanceBreakdown[2]).format()}
+                  precision={2}
+                  valueStyle={{color: 'orange'}}
+                  prefix={<ArrowDownOutlined />}
+                />
+              </Card>
+            </Col>
+          </Row>
         </div>
       )
     }
