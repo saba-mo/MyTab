be6c71e4a8c794961c8307be33d8002c0d3da3ca
Saba Mohtasham Schuttloffel <sabamohtasham@gmail.com>
2021-02-13 12:34:44 -0800

    Feat: styled created form but needs to be centered

--- client/components/expenses/CreateGroupExpenseForm.js
+++ client/components/expenses/CreateGroupExpenseForm.js
@@ -101,19 +101,23 @@ export class CreateGroupExpenseForm extends React.Component {
     }
 
     return (
-      <div className="expense-form-container">
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor="name">Expense Name*</label>
-          <input
-            className="expense-form"
-            type="text"
-            name="name"
-            value={this.state.name}
-            onChange={this.handleChange}
-            placeholder="Ex: Quarantine Brunch"
-          />
-          <div>
-            <label htmlFor="totalCost">Cost* $</label>
+      <div className="expense-page">
+        <div className="expense-form-container">
+          <form onSubmit={this.handleSubmit}>
+            <label htmlFor="name">Expense Name</label>
+            <br />
+            <input
+              className="expense-form"
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+              placeholder="Ex: Quarantine Brunch"
+              required
+            />
+            <br />
+            <label htmlFor="totalCost">Cost</label>
+            <br />
             <input
               className="form-state, expense-form"
               type="number"
@@ -123,84 +127,90 @@ export class CreateGroupExpenseForm extends React.Component {
               value={this.state.totalCost === 0 ? '' : this.state.totalCost}
               onChange={this.handleChange}
               placeholder="Ex: 100 or 9.39"
+              required
             />
-          </div>
-          <label htmlFor="paidBy">Paid By*</label>
-          <select
-            className="expense-form"
-            value={this.state.paidBy}
-            onChange={this.handleChange}
-            name="paidBy"
-          >
-            <option value="member">select</option>
-            {this.props.groupMembers.map((member) => (
-              <option key={`member-${member.id}`} value={member.id}>
-                {member.firstName} {member.lastName}
-              </option>
-            ))}
-          </select>
-          {this.props.groupMembers.map((member) => (
-            <div className="container" key={member.id}>
-              <div className="expense-form">
-                {member.firstName} {member.lastName} owes: $
-                <input
-                  disabled={member.id === this.state.paidBy}
-                  className="expense-form"
-                  min={0}
-                  type="number"
-                  step="0.01"
-                  value={
-                    this.state.owedByMember[member.id] === undefined
-                      ? ''
-                      : this.state.owedByMember[member.id] === 0
-                      ? ''
-                      : this.state.owedByMember[member.id]
-                  }
-                  placeholder={
-                    member.id === this.state.paidBy
-                      ? 'Paid by'
-                      : 'Ex: 100 or 9.39'
-                  }
-                  onChange={(event) =>
-                    this.handleAmountOwedChange(
-                      member.id,
-                      Number(event.target.value)
-                    )
-                  }
-                />
-              </div>
+            <br />
+            <label htmlFor="paidBy">Paid By</label>
+            <br />
+            <select
+              className="expense-form"
+              value={this.state.paidBy}
+              onChange={this.handleChange}
+              name="paidBy"
+              required
+            >
+              <option value="member">select</option>
+              {this.props.groupMembers.map((member) => (
+                <option key={`member-${member.id}`} value={member.id}>
+                  {member.firstName} {member.lastName}
+                </option>
+              ))}
+            </select>
+            <div className="expense-form">
+              {this.props.groupMembers.map((member) => (
+                <div key={member.id}>
+                  {member.firstName} {member.lastName} owes:
+                  <br />
+                  <input
+                    disabled={member.id === this.state.paidBy}
+                    className="expense-form"
+                    min={0}
+                    type="number"
+                    step="0.01"
+                    value={
+                      this.state.owedByMember[member.id] === undefined
+                        ? ''
+                        : this.state.owedByMember[member.id] === 0
+                        ? ''
+                        : this.state.owedByMember[member.id]
+                    }
+                    placeholder={
+                      member.id === this.state.paidBy
+                        ? 'Paid by'
+                        : 'Ex: 100 or 9.39'
+                    }
+                    onChange={(event) =>
+                      this.handleAmountOwedChange(
+                        member.id,
+                        Number(event.target.value)
+                      )
+                    }
+                  />
+                </div>
+              ))}
             </div>
-          ))}
 
-          <div>
-            <div>Total Cost: {currency(this.state.totalCost).format()}</div>
-            <div>Total Owed: {currency(totalOwed).format()}</div>
-            {remainder - totalOwed &&
-            remainder - totalOwed != this.state.totalCost ? (
-              <div className="error">
-                {paidByName} share: {currency(remainder - totalOwed).format()}
-              </div>
-            ) : (
-              ''
-            )}
-          </div>
-          <small className="required">* Required field</small>
-          <button
-            // className="create-expense-button"
-            type="submit"
-            disabled={totalOwed === 0 || totalOwed > this.state.totalCost}
-          >
-            Create Expense
-          </button>
-          {totalOwed > this.state.totalCost && (
-            <div className="error">
-              Total Cost cannot be greater than Total Owed
+            <div>
+              <div>Total Cost: {currency(this.state.totalCost).format()}</div>
+              <div>Total Owed: {currency(totalOwed).format()}</div>
+              {remainder - totalOwed &&
+              remainder - totalOwed != this.state.totalCost ? (
+                <div className="error">
+                  {paidByName} share: {currency(remainder - totalOwed).format()}
+                </div>
+              ) : (
+                ''
+              )}
             </div>
-          )}
-          {totalOwed === 0 && (
-            <div className="error">Total Owed must be greater than 0</div>
-          )}
-        </form>
+            {totalOwed > this.state.totalCost && (
+              <div className="error">
+                Total Cost cannot be greater than Total Owed
+              </div>
+            )}
+            {totalOwed === 0 && (
+              <div className="error">Total Owed must be greater than 0</div>
+            )}
+            <div>
+              <button
+                className="create-expense-button"
+                type="submit"
+                disabled={totalOwed === 0 || totalOwed > this.state.totalCost}
+              >
+                Create Expense
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     )
   }
