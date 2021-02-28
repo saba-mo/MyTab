2350fa9686c0bf07d9bb43087260df8d5df99ebe
Saba Mohtasham Schuttloffel <sabamohtasham@gmail.com>
2021-02-27 16:39:44 -0800

    Feat: group members list responsive

--- client/components/friends/GroupMembers.js
+++ client/components/friends/GroupMembers.js
@@ -7,94 +7,26 @@ import {
   _addGroupMember,
   _deleteGroupMember,
 } from '../../store'
-import {List, Avatar, Button, Skeleton, Menu, Dropdown, message} from 'antd'
+import {List, Avatar, Skeleton, Menu, Dropdown, message} from 'antd'
 import {DownOutlined} from '@ant-design/icons'
-
-// import Button from antd
-// ant example loads 3 (the number stored in count) fake data inputs that are randomly generated every time fakeDataUrl is called
-// const count = 3
-// const fakeDataUrl = `https://randomuser.me/api/?results=${count}&inc=name,gender,email,nat&noinfo`
 
 export class GroupMembers extends React.Component {
   constructor() {
     super()
     this.state = {
       numberOfMembers: 0,
-      // initLoading: true,
-      // loading: false,
-      // data: [],
-      // list: [],
     }
 
     this.attemptToRemoveMember = this.attemptToRemoveMember.bind(this)
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
     this.friendsNotInGroup = this.friendsNotInGroup.bind(this)
-    // this.onLoadMore = this.onLoadMore.bind(this)
   }
 
   componentDidMount() {
     this.props.loadGroupMembers(this.props.groupId)
     this.props.loadFriends(this.props.user.id)
   }
-
-  // Ant process for componentDidMount:
-  // the component mounts with 3 randomly generated names for the data array from fakeDataUrl and they are placed in the list array
-  // componentDidMount() {
-  //   this.getData((res) => {
-  //     this.setState({
-  //       initLoading: false,
-  //       data: res.results,
-  //       list: res.results,
-  //     })
-  //   })
-  // }
-
-  // Ant process for getData:
-  // using an npm package ant requests (reqwest) three randomly generated name sets of data from fakeDataUrl, they are return in json type using api call .get
-  // getData = (callback) => {
-  //   reqwest({
-  //     url: fakeDataUrl,
-  //     type: 'json',
-  //     method: 'get',
-  //     contentType: 'application/json',
-  //     success: (res) => {
-  //       callback(res)
-  //     },
-  //   })
-  // }
-
-  // Ant process for onLoadMore:
-  // sets the state list array with concatinated data aray plus three more seen as loading, while getData is running the stat loading is true.
-  // gets more data from fakeDataUrl (res) then adds to data array, sets the state with the data and updates the list array to equal the data array, once done it updates the loading to false. Lastly a window.dipatchEvent
-
-  // onLoadMore = () => {
-  //   this.setState({
-  //     loading: true,
-  //     list: this.state.data.concat(
-  //       [...new Array(3)].map(() => ({
-  //         loading: true,
-  //         // name: {}
-  //       }))
-  //     ),
-  //   })
-  //   this.getData((res) => {
-  //     const data = this.state.data.concat(res.results)
-  //     this.setState(
-  //       {
-  //         data,
-  //         list: data,
-  //         loading: false,
-  //       },
-  //       () => {
-  //         // Resetting window's offsetTop so as to display react-virtualized demo underfloor.
-  //         // In real scene, you can using public method of react-virtualized:
-  //         // https://stackoverflow.com/questions/46700726/how-to-use-public-method-updateposition-of-react-virtualized
-  //         window.dispatchEvent(new Event('resize'))
-  //       }
-  //     )
-  //   })
-  // }
 
   // filters out user's friends that are already in group so they don't appear in dropdown menu
   friendsNotInGroup(friends, groupMembers) {
@@ -147,21 +79,6 @@ export class GroupMembers extends React.Component {
       this.props.groupMembers || []
     )
 
-    // const {initLoading, loading} = this.state
-    // const loadMore =
-    //   !initLoading && !loading ? (
-    //     <div
-    //       style={{
-    //         textAlign: 'center',
-    //         marginTop: 12,
-    //         height: 32,
-    //         lineHeight: '32px',
-    //       }}
-    //     >
-    //       <Button onClick={this.onLoadMore}>loading more</Button>
-    //     </div>
-    //   ) : null
-
     //drop down menu for choosing a friend to add to the group
     const addFriendMenu = (
       <Menu onClick={this.handleSubmit}>
@@ -180,9 +97,7 @@ export class GroupMembers extends React.Component {
           {this.noMembers(groupMembers)}
           <List
             className="group-member-list"
-            // loading={initLoading}
-            itemLayout="horizontal"
-            // loadMore={loadMore}
+            itemLayout={window.innerWidth < 700 ? 'vertical' : 'horizontal'}
             dataSource={groupMembers}
             renderItem={(item) => (
               <List.Item
