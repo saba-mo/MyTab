119a63323199e95877654c0c01424fb283da05e7
Saba Mohtasham Schuttloffel <sabamohtasham@gmail.com>
2021-02-27 21:35:42 -0800

    Feat: landing page shows login component

--- client/routes.js
+++ client/routes.js
@@ -56,6 +56,7 @@ class Routes extends Component {
         {/* Routes placed here are available to all visitors */}
         {!isLoggedIn && (
           <>
+            <Route path="/" component={Login} />
             <Route path="/login" component={Login} />
             <Route path="/signup" component={Signup} />
           </>
